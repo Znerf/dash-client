3d9d7a2b4f2b1a3d79fd295cdb537ade81fa63f2
Sagar Shrestha <znerfii.a1@gmail.com>
2023-03-29 13:35:28 +0545

    chore(dashboard): add text

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -4,11 +4,13 @@
     <button
       type="button"
       @click="logout"
-      class=" float-right mr-2 inline-block rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase  shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)]  ">
+      class=" float-right mr-2 rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase  shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)]  ">
       Logout
     </button>
+
+    <div class="block">Welcome, user</div>
   
-</section>
+  </section>
 </template>
 
 <script>
